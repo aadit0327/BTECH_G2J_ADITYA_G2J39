7ac0f8682d6f6ea7e9e234700bdab3700aa69816
aadit0327 <25113cn074@glbitm.ac.in>
2026-02-11 11:34:33 +0530

    Add files via upload

---
+++ largestnumber.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+int main ()
+{
+    int a,b,c;
+    printf ("Input number a:\n");
+    scanf("%d",&a);
+    printf("Input number b:\n");
+    scanf("%d",&b);
+     printf("Input number c:\n");
+    scanf("%d",&c);
+    if(a>b,a>c)
+    {
+    printf("A is the Greatest");
+    }
+    else if (b>a,b>c)
+    {
+        printf("B is the Greatest");
+    }
+    else
+    {
+        printf("C is the Greatest");
+    }
+    return 0;
+}
