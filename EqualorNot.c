06019212afc9b6fc9b847f2bf7a323848dd199a2
aadit0327 <25113cn074@glbitm.ac.in>
2026-02-11 11:10:27 +0530

    Add files via upload

---
+++ EqualorNot.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main ()
+{
+    int a,b;
+    printf ("Input number a:");
+    scanf("%d",&a);
+    printf("Input number b:");
+    scanf("%d",&b);
+}
+if (Condition=true)
+printf("a=b")
+{
+    /* code */
+}
+
