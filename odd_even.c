12e0a5958be48ed80b9f4e2162688c69afdbd125
aadit0327 <25113cn074@glbitm.ac.in>
2026-02-11 12:13:33 +0530

    Add files via upload

---
+++ odd_even.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+int main ()
+{
+    int a;
+    printf ("Input number a:\n");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("\n N IS EVEN NUMBER");
+    }
+    else
+    {
+        printf("\n N IS ODD NUMBER");
+    }
+    return 0;
+}
