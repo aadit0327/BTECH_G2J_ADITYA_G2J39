2a0d79fb69c3052eebee284af8c32ef547b8f9af
aadit0327 <25113cn074@glbitm.ac.in>
2026-03-25 10:14:00 +0530

    Create sum_of_diagonals.c

---
+++ sum_of_diagonals.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main ()
+{
+    int a[100][100], r,c,i,j,sum;
+    printf("Enter number of rows : ");
+    scanf("%d", &r);
+    printf("Enter number of columns : ");
+    scanf("%d", &c);
+    for (i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+        {
+            printf("Enter a[%d][%d] = ", i,j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+        printf("Entered Matrix : \n");
+        for(i=0; i<r; i++)
+        {
+            for(j=0; j<c; j++)
+            {
+                printf("%d\t", a[i][j]);
+                sum += a[i][j];
+            }
+            printf("\n");
+        }
+        printf("Sum of Elements of Diagonal is %d", sum);
+        return 0;
+}       
