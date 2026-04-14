f5b0b2ec10b4e0db597c856e4e00749cd13cd992
aadit0327 <25113cn074@glbitm.ac.in>
2026-04-14 15:06:37 +0530

    Create matrix.c

---
+++ Matrix/matrix.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main ()
+{
+    int a[100][100], r,c,i,j;
+    printf("Enter number of rows : ");
+    scanf("%d", &r);
+    printf("Enter number of columns : ");
+    scanf("%d", &c);
+    for (i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+        {
+            printf("Enter a[%d][%d] = ", i,j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+        printf("Entered Matrix : \n");
+        for(i=0; i<r; i++)
+        {
+            for(j=0; j<c; j++)
+            {
+                printf("%d\t", a[i][j]);
+            }
+            printf("\n");
+        }
+        return 0;
+}       
